87fc611c4d42626126c1c5c6e75ca3bb12b4cc30
Nate Totten <nate@zuplo.com>
2022-02-01 09:36:22 -0500

    initial source

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/**@type {import('eslint').Linter.Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "no-console": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-floating-promises": "error",
+  },
+};
---
+++ packages/eslint-config-cloudflare-workers/README.md
@@ -0,0 +1,1 @@
+# eslint-config-cloudflare-workers
